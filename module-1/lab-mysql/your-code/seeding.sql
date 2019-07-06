cd4ceaec83c5d1a2cb574764c1224907e999cf49
Laure Lekieffre <llekieffre@yahoo.fr>
2019-07-06 14:01:55 +0200

    module 1: numpy et pandas - 6 juillet

--- module-1/lab-mysql/your-code/seeding.sql
+++ module-1/lab-mysql/your-code/seeding.sql
@@ -1,5 +1,4 @@
 USE lab_my_sql;
-DROP TABLE 'tables prix et calendrier - copie';
 TRUNCATE TABLE categories;
 INSERT INTO categories (IDCat, LibCat, IDSousCat, LibSousCat) VALUES (1,"FEMME",11,"SHAMPOINGS FEMMES"),
 (1,"FEMME",12,"PARFUMS FEMMES"),
@@ -10,19 +9,32 @@ INSERT INTO categories (IDCat, LibCat, IDSousCat, LibSousCat) VALUES (1,"FEMME",
 (3,"ENFANT",31,"SHAMPOINGS ENFANTS"),
 (3,"ENFANT",32,"PARFUMS ENFANTS"),
 (3,"ENFANT",33,"CREME SOLAIRE ENFANTS");
-
-CREATE TABLE `lab_my_sql`.`prix_litteraire` (
+DROP TABLE lab_my_sql.prix_litterairebis;
+CREATE TABLE lab_my_sql.prix_litterairebis (
 	`EAN` bigint,
     `Titre` text,
     `Auteur` text,
     `Collection` text,
     `Nom prix` text,
     `Annee prix` int,
-    `date prix` text,
+    `date prix` date,
     `Semaine GfK prix` int,
     `Double prix` text
-    )
+    );
+    PREPARE stmt FROM INSERT INTO lab_my_sql.prix_litterairebis (
+    `Annee prix`,
+    `EAN`,
+    `Collection`,
+    `Nom prix`,
+    `Semaine GfK prix`,
+    `Auteur`,
+    `Double prix`,
+    `Titre`
+    ) VALUES (?,?,?,?,?,?,?,?);
+    DEALLOCATE PREPARE stmt;
+
 LOAD DATA LOCAL INFILE 'C:\Users\lleki\Documents\Exercices-Ironhack\ironhack-datalabs-dataparis0619-pt\module-1\lab-mysql\your-code\Source.csv'
   INTO TABLE clients
-  FIELDS TERMINATED BY ','
+  FIELDS TERMINATED BY ';'
   LINES TERMINATED BY '\r\n';
+
