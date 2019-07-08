c542957e591eb960a1975ef5938f050fa0437c73
Laure Lekieffre <llekieffre@yahoo.fr>
2019-07-08 20:54:42 +0200

    module 1 : data cleaning et import - 08-07

--- module-1/lab-mysql-select/your-code/challenge2.sql
+++ module-1/lab-mysql-select/your-code/challenge2.sql
@@ -1,9 +1,9 @@
 USE publications;
 
-SELECT a.au_id AS Author_ID, a.au_lname AS Author_LastName, a.au_fname AS Author_FirstName, p.pub_name AS Publisher, COUNT(*) AS Nb_Titres
+SELECT a.au_id AS Author_ID, a.au_lname AS Author_LastName, a.au_fname AS Author_FirstName, p.pub_name AS Publisher, SUM(s.qty) AS "Qté"
 FROM publications.authors a
 JOIN publications.titleauthor ta ON a.au_id = ta.au_id
 JOIN publications.titles t ON t.title_id = ta.title_id
 JOIN publications.publishers p ON p.pub_id = t.pub_id
-GROUP BY t.title_id;
+GROUP BY p.pub_id, a.au_id;
 SELECT COUNT(*) FROM publications.titles;
